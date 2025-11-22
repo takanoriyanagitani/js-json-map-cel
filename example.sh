71c1d3a3b58ed169c61690cad37926020438cc56
yanagitani <takanori.yanagitani@gmail.com>
2025-11-23 05:43:35 +0900

    docs: example to use the cel expr

---
+++ example.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+ex1() {
+	echo "--- Running Example 1 (all conditions should pass) ---"
+	jq -c -n '{
+    time: "2025-11-20T07:13:46.012Z",
+    severity: "INFO",
+    status: 200,
+    body: "apt update done",
+  }' | EXPR_STRING='{
+    "time_ok": 0 < input.time.size(),
+    "severity_ok": input.severity in ["INFO", "WARN", "FATAL"],
+    "status_ok": 100 <= input.status && input.status < 600,
+    "body_ok": 0 < input.body.size(),
+  }' node ./index.mjs
+}
+
+ex2() {
+	echo "\n--- Running Example 2 (severity should fail) ---"
+	jq -c -n '{
+    time: "2025-11-20T07:13:46.012Z",
+    severity: "DEBUG",
+    status: 200,
+    body: "apt update done",
+  }' | EXPR_STRING='{
+    "time_check": 0 < input.time.size() ? "ok" : "invalid time string: " + input.time,
+    "severity_check": input.severity in ["INFO", "WARN", "FATAL"]
+        ? "ok"
+        : "invalid severity string: " + input.severity,
+    "status_ok": string(100 <= input.status && input.status < 600),
+    "body_ok": string(0 < input.body.size()),
+  }' node ./index.mjs
+}
+
+ex1
+ex2
